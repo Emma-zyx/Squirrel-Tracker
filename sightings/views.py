850c2e829c575575f9ac905b856f2013ef6c83ab
Yiran Lin <yl4600@columbia.edu>
2020-10-16 20:33:17 +0000

    Oct-16 Afternoon

--- sightings/views.py
+++ sightings/views.py
@@ -17,3 +17,28 @@ def map(request):
     context = {'sightings': mapped}
     return render(request, 'sightings/map.html',context)
 
+def add(request):
+    if request.method == 'POST':
+        form = TrackingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/sightings/')
+    else:
+        form = TrackingForm()
+    context = {'form': form}
+    return render(request, 'sightings/add.html',context)
+
+def stats(request):
+    sq_num = Squirrel.objects.all().count()
+    sq_adult = Squirrel.objects.filter(Age='Adult').count()
+    sq_cinna = Squirrel.objects.filter(Primary_fur_color='Cinnamon').count()
+    sq_chasing = Squirrel.objects.filter(Chasing=True).count()
+    sq_tail_twitches = Squirrel.objects.filter(Tail_twitches=True).count()
+
+    context = {'sq_num': sq_num,
+               'sq_adult': sq_adult,
+               'sq_cinna': sq_cinna,
+               'sq_chasing': sq_chasing,
+               'sq_tail_twitches': sq_tail_twitches}
+
+    return render(request, 'sightings/stats.html',context)
