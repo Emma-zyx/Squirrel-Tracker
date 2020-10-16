837741f595baa3d0d50341f59399570cd89c677f
Yiran Lin <yl4600@columbia.edu>
2020-10-16 18:29:27 +0000

    Oct 16 - Morning

--- sightings/url.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-apps_name = 'sightings'
-
-urlpatterns = [
-   path('', views.squirrels_list, name = 'squirrels_list')
-]
-
--- sightings/urls.py
+++ sightings/urls.py
@@ -1,10 +1,12 @@
+
 from django.urls import path
 from . import views
 
 apps_name = 'sightings'
 
-urlpatterns = [
-   path('', views.squirrels_list, name = 'squirrels_list'),
+urlpatterns =[
+   path('sightings/', views.squirrels_list, name = 'squirrels_list'),
    path('map/',views.map),
+   path('sightings/add/', views.add),
+   path('sightings/stats/',views.stats),
 ]
-
