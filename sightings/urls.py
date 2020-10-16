0c6560d7ee4fa01ee6a677c457ace7ad0c271238
Emma Zhou <emma.zhou@columbia.edu>
2020-10-16 20:41:13 +0000

    add forms.py and add update function

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,6 +4,7 @@ from . import views
 apps_name = 'sightings'
 
 urlpatterns = [
-   path('', views.squirrels_list, name = 'squirrels_list')
+   path('sightings/', views.squirrels_list, name = 'squirrels_list'),
+   path('sightings/<str:squirrel_id>/', views.update_squirrel, name='update'),
 ]
 
--- sightings/views.py
+++ sightings/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.contrib import messages
 from .models import Squirrel
+from .forms import TrackingForm
 
 def squirrels_list(request):
   squirrels = Squirrel.objects.all()
   context = {'squirrels': squirrels}
   return render(request, 'sightings/squirrel_list.html', context)
+
+def update_squirrel(request, squirrel_id):
+    squirrel = Squirrel.objects.get(Unique_Squirrel_ID = squirrel_id)
+    
+    if request.method == 'POST':
+        form = TrackingForm(request.POST, instance=squirrel)
+        if form.is_valid():
+            form.save()
+            message.success(request, "Updated successfully!")
+            return redirect(f'/sightings/')
+    else:
+        form = TrackingForm(instance=squirrel)
+        
+    context = {'form':form,}
+    return render(request, 'sightings/update.html',context)
+
 
