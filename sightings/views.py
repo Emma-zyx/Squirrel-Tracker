b04847c9f316afa5ab3300ab6a6eccb0e6237842
Yiran Lin <yl4600@columbia.edu>
2020-10-17 05:23:51 +0000

    Seems that we did it!

--- sightings/views.py
+++ sightings/views.py
@@ -28,7 +28,7 @@ def update_squirrel(request, squirrel_id):
 
 def map(request):
     mapped = Squirrel.objects.all()[:100]
-    context = {'sightings': mapped}
+    context = {'mapped': mapped}
     return render(request, 'sightings/map.html',context)
 
 def add(request):
